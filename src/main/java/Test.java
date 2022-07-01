609ad3eb39807a072efe19ed73e3a262518227a4
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-07-01 11:18:09 -0500

    Maven added

---
+++ src/main/java/Test.java
@@ -0,0 +1,23 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("Enter something:");
+        Scanner scan = new Scanner(System.in);
+
+        String userInput = scan.nextLine();
+        String flipped = "";
+        String revFlip = "";
+
+        if(StringUtils.isNumeric(userInput)){
+            System.out.println("You entered a number!");
+        } else {
+            revFlip = StringUtils.reverse(StringUtils.swapCase(userInput));
+        }
+        System.out.println(revFlip);
+    }
+}
